719dedbc021ebb101ca965d0857eb1c11bc65a5b
herteljt <herteljt@gmail.com>
2022-03-26 16:53:14 -0500

    test commit with balloon sample

---
+++ main.lua
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local background = display.newImageRect("background.png", 360, 570)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect( "platform.png", 300, 50)
+platform.x = display.contentCenterX
+platform.y = display.contentHeight-25
+
+local balloon = display.newImageRect("balloon.png", 112, 112)
+balloon.x = display.contentCenterX
+balloon.y = display.contentCenterY
+balloon.alpha = 0.8
