1b714388b925d4071d191e481506901f57b32261
herteljt <herteljt@gmail.com>
2022-03-26 18:51:52 -0500

    Trying Star Explorer Example

--- config.lua
+++ config.lua
@@ -7,11 +7,11 @@ application =
 {
 	content =
 	{
-		width = 320,
-		height = 480, 
-		scale = "letterbox",
+		width = 768,
+		height = 1024,
+		scale = "zoomEven",
 		fps = 60,
-		
+
 		--[[
 		imageSuffix =
 		{
